3ca5ef26eb5d0ee18fb4695b07515855d650a1ca
Neutrollized <glen.yu@gmail.com>
2020-08-03 12:58:33 -0400

    updated default react page link

---
+++ build/precache-manifest.50b01f515ea10ad061e51405d3f53c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a508c2c3ac0551e1e8de93a2893dbc35",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "cf2372a170a0bf136b78",
+    "url": "/react-blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b45aaff03ac9699c404",
+    "url": "/react-blog/static/js/2.0d7d43cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-blog/static/js/2.0d7d43cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2372a170a0bf136b78",
+    "url": "/react-blog/static/js/main.d81672d5.chunk.js"
+  },
+  {
+    "revision": "0bca87922476c71d01b6",
+    "url": "/react-blog/static/js/runtime-main.796b35e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Neutrollized"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to my GitHub profile page
         </a>
       </header>
     </div>
